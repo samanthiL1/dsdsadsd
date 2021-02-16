72090e30b1868f35f0239f4e733591a40645da38
Samanthi <samanthilw44@gmail.com>
2021-02-16 11:56:20 +0530

    first ui

---
+++ components/index.js
@@ -0,0 +1,15 @@
+import Button from './Button';
+import DrawerItem from './DrawerItem';
+import Icon from './Icon';
+
+import Input from './Input';
+
+
+export {
+  Button,
+  
+  DrawerItem,
+  Icon,
+  Input,
+ 
+};
